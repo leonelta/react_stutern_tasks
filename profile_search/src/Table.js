6fb00e7ce21de63fbad892e18d45c2ee911aea7e
leonelta <ketchakaren@gmail.com>
2023-04-16 05:43:15 +0200

    styled all the cards

--- profile_search/src/Table.js
+++ profile_search/src/Table.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import styles from './table.module.css';
-
+import React from 'react';
+import './table.css';
 const table = ({data}) => {
   return (
-    <div className="container">
-      {data.map((item) => (
-         <div className='card' key={item.id}>
-            <div className='image_card'>
-              <img src={item.picture}alt=''/>
-            </div>
-            <div className='text_card'>
-              <h1>{item.id}</h1>
-              <div className="text">
-                  <p>{item.title}</p>
-                  <p>{item.firstName}</p>
-                  <p>{item.lastName}</p>
+    <div>
+      <section>
+        <div className='container'>
+          <div className='cards'>
+            {data.map((item) => (
+              
+              <div className='text_card' key={item.id}>
+                <div className='image_card'>
+                   <img src={item.picture} alt=''/>
+                </div>
+                <div className="text">
+                   <h5>{item.id}</h5>
+                    <p>{item.title} {item.firstName} {item.lastName}</p>
+                </div>
               </div>
-            </div>
-         </div>
-      ))}
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
