1c4009dffb342e4691d6acb29a8e3be31c7c290a
leonelta <ketchakaren@gmail.com>
2023-04-27 13:44:55 +0200

    used the if condition to check if the email is entered if it is not it should return email required and also if is a valid one

--- forms_project/src/validateInfo.js
+++ forms_project/src/validateInfo.js
@@ -6,5 +6,13 @@ export default function validateInfo(data) {
         errors.name = "Name Required"
     }
 
+    //Email field
+
+    if(!data.email) {
+        errors.email = "Email required"
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(data.email)) {
+        errors.email = "Email address is invalid"
+    }
+
 
 }
