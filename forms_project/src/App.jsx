f084c251b955863528fa856b1bc500c4e8639858
leonelta <ketchakaren@gmail.com>
2023-04-27 06:11:43 +0200

    created a form using the form tag, input, label and textarea tag and styled all of them

--- forms_project/src/App.jsx
+++ forms_project/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React,{ useState } from 'react'
 import './App.css'
+import Form from './Form'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Form />
     </div>
   )
 }
--- forms_project/src/Form.jsx
+++ forms_project/src/Form.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+
 
 function Form() {
+
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+    const [errorMessages, setErrorMessages] = useState([]);
+    const [showErrors, setShowErrors] = useState(false);
+
   return (
-    <div>Form</div>
+    <div  className='container'>
+        <form> 
+            <h1>Contact Us</h1>
+            <div className="form-inputs">
+              <label htmlFor="name" className="form-label">Name</label> 
+              <input type="text" name="Name" id="" className="form-input" placeholder="Enter your name" />
+            </div>
+            <div className="form-inputs">
+              <label htmlFor="email" className="form-label">Email</label> 
+              <input type="email" name="Email" id="" className="" placeholder="Enter your email"/>
+            </div>
+            <div className="form-inputs">
+              <label htmlFor="subject" className="form-label">Subject</label> 
+              <input type="text" name="Subject" id="" className="" placeholder="Enter your subject"/>
+            </div>
+            <div className="form-inputs">
+              <label htmlFor="message" className="form-label">Message</label> 
+              <textarea name="Message" id="" className="" cols={20} rows={10} placeholder="Type here ..." />
+            </div>
+            <button className="bottom" type="submit">Submit</button>
+        </form>
+    </div>
   )
 }
 
