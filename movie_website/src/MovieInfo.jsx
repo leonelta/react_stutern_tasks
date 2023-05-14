b26d97d969c882dba2879d8bfe9fbccd0d0e860b
leonelta <ketchakaren@gmail.com>
2023-05-14 16:34:32 +0200

    added the different props to file

--- movie_website/src/MovieInfo.jsx
+++ movie_website/src/MovieInfo.jsx
@@ -1,55 +1,118 @@
 import React, {useState, useEffect} from 'react';
 import { useNavigate, useParams } from 'react-router';
+import axios from 'axios';
 
-function MovieInfo(title, director, producer, opening_crawl, Species, characters, Starships, planets, Vehicles) {
+function MovieInfo() {
 
   const { episode_id } = useParams();
   const navigate = useNavigate();
   const [context, setContext] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const navigateToMovieBox = () => {
     navigate('/MovieBox');
   }
-  
+
+  const fetchData = async(urls) => {
+    const results = await Promise.all(urls.map((url) => axios.get(url)));
+    return results.map((res) = res.data);
+  }
 
   useEffect(() => {
     const LoadInfo = async () => {
+      try {
         setLoading(true);
-
         const response = await axios.get(
-           "https://swapi.dev/api/films" 
-        )
+           "https://swapi.dev/api/films/{$episode_id}" 
+        );
+        const Info = response.data;
 
-        setContext(response.data)
+        const [characters, species, planets, starships, vehicles] = await Promise.all([
+          fetchData(Info.characters),
+          fetchData(Info.species),
+          fetchData(Info.planets),
+          fetchData(Info.starships),
+          fetchData(Info.vehicles),
+        ]);
 
-        setLoading(false);
+        setContext({
+          ...Info,
+          characters,
+          species,
+          planets, 
+          starships, 
+          vehicles,
+        });
+        console.log(context);
+      } catch (err) {
+        console.error(err.message);
+        setContext(null);
+      } finally {
+        setLoading(false)
+      }
     }
 
     LoadInfo();
-  }, [])
+  }, [episode_id]);
 
   return (
     <div className='container'>
-      <button onClick={navigateToMovieBox}> Back to List</button>
-      <h1 className='title'> {title}</h1> 
-      <p className='direct'><span>Director: {director}</span></p>
-      <p className='producer'><span>Director: {producer}</span></p> 
-      <h3>Description</h3>
-      <p>{opening_crawl}</p>   
-      <h3>Characters</h3>
-      <ul>
-        {context.characters.map(character => {
-          <li key={character.url}>{character.name}</li>
-        })}
-      </ul>
-      <h3>Planets</h3>
-        {context.planets.map(planet => {
-          <li key={planet.url}>{planet.name}</li>
-        })}
-      <h3>Species</h3>
-      <h3>Starships</h3>
-      <h3>Vehicles</h3>
+      {
+        loading ? (
+           <h4>...</h4>
+        ) : (
+           <div className='head'>
+            <button onClick={navigateToMovieBox}> Back to List</button>
+              <h1 className='title'> {context.title}</h1> 
+              <p className='direct'><span>Director: {context.director}</span></p>
+              <p className='producer'><span>Director: {context.producer}</span></p> 
+              <div>
+                <h3>Description</h3>
+                <p>{context.opening_crawl}</p>
+              </div>
+              <div>
+                <h3>Characters</h3>
+                <ul>
+                    {context.characters.map((character) => {
+                      <li key={character.url}>{character.name}</li>
+                    })}
+                </ul>
+              </div>
+              <div>
+               <h3>Planets</h3>
+                <ul>
+                  {context.planets.map((planet) => {
+                    <li key={planet.url}>{planet.name}</li>
+                  })}
+                </ul>
+              </div>
+              <div>
+               <h3>Species</h3>
+                <ul>
+                  {context.species.map((specie) => {
+                    <li key= {specie.url}>{specie.name}</li>
+                  })}
+                </ul>
+              </div>
+              <div>
+               <h3>Starships</h3>
+                <ul>
+                  {context.starships.map((starship) => {
+                    <li key={starship.url}>{starship.name}</li>
+                  })}
+                </ul>
+              </div>
+              <div>
+               <h3>Planets</h3>
+                <ul>
+                  {context.vehicles.map((vehicle) => {
+                    <li key={vehicle.url}>{vehicle.name}</li>
+                  })}
+                </ul>
+              </div>
+           </div>
+        )
+      }
     </div>
   )
 }
